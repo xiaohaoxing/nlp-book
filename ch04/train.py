37c19bb9ffd131818e1316d1cc5c2207a4113a02
xiaohaoxing <xiaohaoxing@xiaomi.com>
2021-03-18 18:03:59 +0800

    完成第四章 CBOW 模型的训练过程代码，需要 GPU 运行加速

---
+++ ch04/train.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+
+import sys
+
+sys.path.append('..')
+import numpy as np
+from common.layers import Embedding, SigmoidWithLoss
+import pickle
+from common.trainer import Trainer
+from dataset import ptb
+from ch04.cbow import CBOW
+from common.optimizer import Adam
+from common.util import create_contexts_target
+
+# 设定超参数
+window_size = 5
+hidden_size = 100
+batch_size = 100
+max_epoch = 10
+
+# 读取数据
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+vocab_size = len(word_to_id)
+
+context, target = create_contexts_target(corpus, window_size)
+
+# 构建模型
+model = CBOW(vocab_size, hidden_size, window_size, corpus)
+optimizer = Adam()
+trainer = Trainer(model, optimizer)
+
+# 开始训练
+trainer.fit(context, target, max_epoch, batch_size)
+trainer.plot()
+
+# 保存模型
+word_vecs = model.word_vecs
+params = {}
+params['word_vecs'] = word_vecs
+params['word_to_id'] = word_to_id
+params['id_to_word'] = id_to_word
+pkl_file = 'cbow_params.pkl'
+with open(pkl_file, 'wb') as f:
+    pickle.dump(params, f, -1)
--- common/layers.py
+++ common/layers.py
@@ -113,3 +113,47 @@ class Sigmoid:
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
         return dx
+
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+
+        np.add.at(dW, self.idx, dout)
+
+        return None
+
+
+class SigmoidWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = 1 / (1 + np.exp(-x))
+
+        self.loss = cross_entropy_error(np.c_(1 - self.y, self.y), self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = (self.y - self.t) * dout / batch_size
+
+        return dx
